53ddda312cfa9bc621c65e4ef749ecc0bd2fd7a2
rekhaMysense <rekha.a@truinc.in>
2023-12-11 15:15:54 +0530

    Commiting changes..

--- wp-content/plugins/git_main/app/view/repos.php
+++ wp-content/plugins/git_main/app/view/repos.php
@@ -108,13 +108,14 @@
                            $class='active';
                          }
                          ?> 
-                     <li class="list-group-item <?php echo $class; ?>"><a href="<?= $active_repo_url ?>" class="repo-anchr"><?php echo $repo['name']; ?></a></li>
-                     <?php    } ?>
+                     <!-- <li class="list-group-item <?php echo $class; ?>"><a href="<?= $active_repo_url ?>" class="repo-anchr"><?php echo $repo['name']; ?></a></li> -->
+                     <li class="list-group-item <?php echo $class; ?>"><a href="javascript:void(0);" class="repo-anchr"><?php echo $repo['name']; ?></a></li>
+                    <?php    } ?>
                   </ul>
                </div>
             </div>
             <div class="col-md-5" style="text-align:center">
-               <h1><?php echo $current_repo?'Current repo - '.$current_repo:'No active repository' ?> </h1>
+               <h1><?php echo $current_repo?'Your Linked repo - '.$current_repo:'No active repository' ?> </h1>
             </div>
             <?php  endif; ?>
          </div>
@@ -210,7 +211,12 @@
 
                         <button type="button" class="btn btn-primary btn-lg git-pull-request">Pull changes</button>
                         <button type="button" class="btn btn-primary btn-lg check-git-status">Check status</button>
-                        <button type="button" class="btn btn-primary btn-lg git-push-request">Push changes</button>
+                        <!-- <button type="button" class="btn btn-primary btn-lg git-push-request">Push changes</button> -->
+                        <button type="button" class="btn btn-primary btn-lg" data-bs-toggle="modal" data-bs-target="#pushChangesModal">
+                        Push changes
+                        </button>
+
+
                      </div>
                      <div class="div-center results-div"></div>
                   </div>
@@ -226,4 +232,27 @@
       </div>
    </div>
 </div>
+
+<div class="modal fade" id="pushChangesModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="staticBackdropLabel">Commit Message</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+
+      <div class="mb-3">
+      <label for="exampleFormControlTextarea1" class="form-label">Description</label>
+      <textarea class="form-control" name="commit_msg" id="commit_msg" rows="3"></textarea>
+      </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary git-push-request">Commit & Push</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 <script></script>
